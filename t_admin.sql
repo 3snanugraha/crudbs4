7a35a13164315b17f30b4f9f7f49f4f9fe9df3e8
Trisna Nugraha <36766348+3snanugraha@users.noreply.github.com>
2019-10-30 13:37:54 +0700

    Add files via upload

---
+++ t_admin.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 30, 2019 at 07:34 AM
+-- Server version: 10.1.37-MariaDB
+-- PHP Version: 7.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_tris`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `t_admin`
+--
+
+CREATE TABLE `t_admin` (
+  `id_admin` int(16) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `nama_admin` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `t_admin`
+--
+
+INSERT INTO `t_admin` (`id_admin`, `username`, `password`, `nama_admin`) VALUES
+(2, '3snanugraha', 'admin123', 'Tris');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `t_admin`
+--
+ALTER TABLE `t_admin`
+  ADD PRIMARY KEY (`id_admin`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `t_admin`
+--
+ALTER TABLE `t_admin`
+  MODIFY `id_admin` int(16) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
